70a19fb95b87227bdb7ada0dbaa5af9bc592fcda
Sohil Lochan <sohil09956@gmail.com>
2025-06-20 22:42:33 +0530

    Updated my About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,12 @@ function About() {
                     <div className="h-[90%] container flex flex-col justify-center items-start bg-black mx-auto rounded-[15px] bg-cover  bg-center sm:bg-contain bg-no-repeat sm:bg-right" style={{ backgroundImage: `url(${my_pic})` }}>
                         <div className="h-full flex flex-col justify-center items-start bg-gradient-to-r from-[rgba(0,0,0,0.9)] lg:from-[rgba(0,0,0,0.7)] to-[rgba(0,0,0,0.3)] rounded-[15px]">
                             <h1 className="mt-8 text-[32px]  text-white w-full text-center decoration-red-500 underline underline-offset-8 text-2xl">About Me</h1>
-                            <p className="w-[90%] h-[80%] sm:w-[45%] ml-4 text-start  mt-3 sm:ml-8 sm:mt-8 lg:mt-28 text-white overflow-auto">I am a passionate coder with a strong affinity for problem-solving, currently exploring the MERN stack for web development. I have completed multiple projects that showcase my skills and creativity, enabling me to build functional and visually appealing applications. My primary programming language is Java, which I prefer for its versatility and performance. I’m excited to continue learning and growing in the tech field, pushing the boundaries of what I can create.</p>
+                            <p className="w-[90%] h-[80%] sm:w-[45%] ml-4 text-start  mt-3 sm:ml-8 sm:mt-8 lg:mt-28 text-white overflow-auto">I’m a passionate full-stack developer with a strong foundation in Java and a growing expertise in the MERN stack (MongoDB, Express.js, React, Node.js). I thrive on solving real-world problems through code and love turning ideas into scalable, user-friendly applications.
+
+                                Over the past year, I’ve built several projects ranging from static frontend websites to full-stack applications with user authentication, API integration, and responsive design. My most recent project, Job Seek, is a complete job search platform that reflects my ability to architect and execute end-to-end solutions.
+
+                                While Java remains my go-to language for its versatility and depth, I actively work with JavaScript, React, and modern web technologies to stay current in the fast-evolving dev world. I'm always excited to learn new tools, collaborate with other developers, and build software that makes an impact.
+                            </p>
                         </div>
                     </div>
                 </div>
